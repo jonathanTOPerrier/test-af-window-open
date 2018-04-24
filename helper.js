3cd807c711e97b4db75cd0e2742ca8992fe61719
jonathanTOPerrier <35562503+jonathanTOPerrier@users.noreply.github.com>
2018-04-24 14:38:22 +0200

    test af kald fra andet window

--- helper.js
+++ helper.js
@@ -3,8 +3,24 @@ document.querySelector('#login').addEventListener('click', (event) => {
 
     event.preventDefault();
 
+
+
     let ny_window = window.open('', 'testeren', 'width=300, height=200 ');
     ny_window.document.write(`<button id="knap">click mig</button>
-    <script>document.querySelector('#knap').addEventListener('click', () => { window.opener.sessionStorage.setItem('test', 'sucess'); window.close()});</script>`)
+    <script>
+    
+    let hej = 'hejsa';
+
+        document.querySelector('#knap').addEventListener('click', () => {  
+
+            window.opener.teksten(hej);
+
+        window.close()});
+        
+        </script>`)
 
 });
+
+// window.opener.sessionStorage.setItem('test', 'sucess');
+
+
